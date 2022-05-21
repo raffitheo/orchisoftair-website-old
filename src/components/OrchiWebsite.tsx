1991f8e94e87d446837c0cd1013ff923b14d74e3
Raffaele Valenti <raffaele-valenti@libero.it>
2022-05-21 13:38:36 +0200

    Added styled components

--- src/components/OrchiWebsite.tsx
+++ src/components/OrchiWebsite.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.svg';
 
 import './OrchiWebsite.css';
 
-function OrchiWebsite() {
+export const OrchiWebsite: React.FC = () => {
     return (
         <div
             className='App'
@@ -34,5 +34,3 @@ function OrchiWebsite() {
         </div>
     );
 }
-
-export default OrchiWebsite;
